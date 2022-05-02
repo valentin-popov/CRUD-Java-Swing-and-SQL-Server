fbeaf297e0308b7dec13b9fbb418636f42f0f475
Valentin <71670019+valentin-popov@users.noreply.github.com>
2022-05-02 19:11:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,21 @@ import javax.swing.*;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.*;
+
 public class Main {
     static Connection con;
     PreparedStatement pst;
     public static void main(String[] args) throws IOException {
-        Connect();
-        //Home home = new Home();
+       Connect();
        Login login = new Login();
     }
 
     public static void Connect() {
         try {
-            //Class.forName("com.mysql.cj.jdbc.Driver");
-            //con = DriverManager.getConnection("jdbc:mysql://localhost/supermarket", "root", "!Database123");
-            SQLServerDataSource ds = new SQLServerDataSource();
-            ds.setUser("SA");
-            ds.setPassword("!Database123");
-            ds.setServerName("localhost");
-            ds.setPortNumber(1433);
-            ds.setDatabaseName("supermarket");
-            con = ds.getConnection();
+            String url = "jdbc:sqlserver://localhost:1433;database=supermarket;encrypt=true;trustServerCertificate=true;";
+            String user = "SA";
+            String password = "!Database123";
+            con = DriverManager.getConnection(url, user, password);
             System.out.println("Connected!");
         }
 
